cdbbddaaa391eac237ff8d00f1e46edb86bd66ec
Ankita <you@gmail.com>
2024-06-25 11:30:30 +0530

    done

---
+++ demo.java
@@ -0,0 +1,12 @@
+import java lang*
+import java until scanner;
+class test{
+public static void main(String args[])
+{
+  int x=9;
+  int y=8;
+  y=x+y-(x=y);
+  System.out.println("x="+x);
+  System.out.println("y="+y);
+  }
+}
